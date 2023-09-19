1f2ff0acf4f8af59c3f4355ef6c1e8fb47492d5e
okano sanaka <sanaka.7hex.bss@gmail.com>
2023-09-20 00:43:01 +0900

    実装途中です

--- src/main/java/com/example/entity/Library.java
+++ src/main/java/com/example/entity/Library.java
@@ -21,6 +21,12 @@ public class Library {
 		@Column(name = "NAME")
 		private String name;
 		
+		@Column(name = "EMAIL")
+		private String email;
+		
+		@Column(name = "PASSWORD")
+		private String password;
+		
 		public Integer getId() {
 	        return this.id;
 	    }
@@ -36,5 +42,20 @@ public class Library {
 	    public void setName(String name) {
 	        this.name = name;
 	    }
+	    
+	    public String getEmail() {
+	    	return this.email;
+	    }
 		
+	    public void setEmail(String email) {
+	    	this.email = email;
+	    }
+	    
+	    public String getPassword() {
+	    	return this.password;
+	    }
+		
+	    public void setPassword(String password) {
+	    	this.password = password;
+	    }
 }
