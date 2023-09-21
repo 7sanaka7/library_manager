7305f7635083403c3ecfc24e5f3dc669ffe3943e
okano sanaka <sanaka.7hex.bss@gmail.com>
2023-09-21 23:08:12 +0900

    SecurityConfig追加

--- src/main/java/com/example/config/SecurityConfig.java
+++ src/main/java/com/example/config/SecurityConfig.java
@@ -1,10 +1,12 @@
 package com.example.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -32,5 +34,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	    public void configure(WebSecurity web) throws Exception {
 	       
 	        web.ignoring().antMatchers("/css/**", "/js/**");
+	    }
+	 
+	 // ハッシュアルゴリズムの定義
+	    @Bean
+	    public PasswordEncoder passwordEncoder() {
+	        return new BCryptPasswordEncoder();
 	    }
 }
